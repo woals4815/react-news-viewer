e887d2be3bf67258011ed886ceb7a352d03dc6b6
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-29 11:24:39 +0900

    Add custom hooks for promise

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from '../../node_modules/axios/index';
+import usePromise from '../lib/usePromise';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -17,30 +18,22 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2a55b72f1d85477ca532ad1075b1e853`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2a55b72f1d85477ca532ad1075b1e853`,
+    );
   }, [category]);
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
-  if (!articles) {
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
